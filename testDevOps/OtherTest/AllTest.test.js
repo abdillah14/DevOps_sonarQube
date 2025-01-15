180d4cecde783212aaabe3015fba8137244da424
Souverain joseph <158466435+Souverain08@users.noreply.github.com>
2025-01-15 07:39:14 +0100

    .

--- testDevOps/OtherTest/AllTest.test.js
+++ testDevOps/OtherTest/AllTest.test.js
@@ -2,7 +2,7 @@ const { sub, factor } = require("./AllTest");
 
 describe("La somme de deux nombre a et b ", () => {
   it("sustraction", () => {
-    expect(sub(16, 4)).toBe(12);
+    expect(sub(14, 4)).toBe(10);
   });
   test("factorielle", () => {
     expect(factor(3)).toBe(6);
